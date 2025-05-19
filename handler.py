00a74b28f32d58e8e195b7dbbfe6c236775518d7
Laion <83224375+laion01@users.noreply.github.com>
2025-05-19 20:42:05 +0000

    image generation

--- handler.py
+++ handler.py
@@ -4,7 +4,99 @@ import uuid
 import base64
 import b2sdk.v2 as b2
 from pathlib import Path
+import os
+from time import time
+from PIL import Image
+import torch
+from diffusers import DiffusionPipeline
+from typing import Tuple
+from datetime import datetime
 
+default_negative = os.getenv("default_negative","")
+
+style_list = [
+    {
+        "name": "3D Model",
+        "prompt": "{prompt}, centered, full view, isolated on white background, no cropping, product render style",
+        "negative_prompt": "",
+    },
+]
+
+styles = {k["name"]: (k["prompt"], k["negative_prompt"]) for k in style_list}
+STYLE_NAMES = list(styles.keys())
+DEFAULT_STYLE_NAME = "3D Model"
+
+def apply_style(style_name: str, positive: str, negative: str = "") -> Tuple[str, str]:
+    p, n = styles.get(style_name, styles[DEFAULT_STYLE_NAME])
+    
+    if not negative:
+        negative = ""
+    p = p.replace("{prompt}", positive)
+    return p, n + negative
+
+USE_TORCH_COMPILE = os.getenv("USE_TORCH_COMPILE", "0") == "1"
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+NUM_IMAGES_PER_PROMPT = 1
+
+if torch.cuda.is_available():
+    pipe = DiffusionPipeline.from_pretrained(
+        "SG161222/RealVisXL_V5.0",
+        torch_dtype=torch.float16,
+        use_safetensors=True,
+        add_watermarker=False,
+        variant="fp16"
+    )
+    pipe.to("cuda")
+    
+    if USE_TORCH_COMPILE:
+        pipe.unet = torch.compile(pipe.unet, mode="reduce-overhead", fullgraph=True)
+
+def save_image(img, output_path):
+    img.save(output_path)
+    return output_path
+
+def generate_image(
+    prompt: str, seed: int = 0, n = 1
+):
+    prompt = prompt + ", centered, full view, 3D isolated on white background, no cropping, product render style"
+    negative_prompt = "blur,stratch"
+    generator = torch.Generator().manual_seed(0)
+
+    options = {
+        "prompt": prompt,
+        "negative_prompt": negative_prompt,
+        "width": 1024,
+        "height": 1024,
+        'seed': seed,
+        "guidance_scale": 20,
+        "num_inference_steps": 25,
+        "generator": generator,
+        "num_images_per_prompt": n,
+        "use_resolution_binning": True,
+        "output_type": "pil",
+    }
+
+    images = pipe(**options).images
+    saved_files = []
+    for i, item in enumerate(images):
+        filename = datetime.now().strftime("/workspace/james/images/RealVisXL_%Y%m%d_%H%M%S") + f"_base_{i+1}.png"
+        item.save(filename)
+        saved_files.append(filename)
+        print(f"Saved: {filename}")
+
+    if n==1:
+        return saved_files[0]
+    return saved_files
+
+if __name__ == "__main__":
+    t1 = time()
+    i = generate_image("Coffee table with a pile of magazines on top, centered, full view, isolated on white background, no cropping, product render style")
+    # i = generate_image("airplane hangar building, centered, full view, isolated on white background, no cropping, product render style, outside view")
+
+    print(f"{time() - t1} seconds")
+    
 # --- B2 Setup ---
 # B2_APPLICATION_KEY_ID = os.getenv("B2_APPLICATION_KEY_ID")
 # B2_APPLICATION_KEY = os.getenv("B2_APPLICATION_KEY")
@@ -42,11 +134,12 @@ def handler(job):
     prompt = job_input.get("prompt", "")
 
     image_path = video_path = None
-
+    images = generate_image(prompt)
     try:
 
         return {
             "prompt": prompt,
+            "imageFile": images
         }
 
     except Exception as e:
